4caf218c2f73574594549741aa43a2b6eba28d6f
honguyenminh <honguyenminh@outlook.com>
2021-07-31 22:19:28 +0700

    Change UI. Add ObservableDict.

--- VotingDatabaseMaker/MainWindow.xaml.cs
+++ VotingDatabaseMaker/MainWindow.xaml.cs
@@ -27,21 +27,23 @@ namespace VotingDatabaseMaker
         private readonly Dialogs dialogs;
         private readonly CandidateDialog candidateDialog = new();
         //private static SQLiteAsyncConnection connection;
-        private readonly Dictionary<string, Dictionary<string, Candidate>> candidates = new(1);
-        private readonly SectorDictionary sectorDict = new();
+        private readonly ObservableDictionary<string, Dictionary<string, Candidate>> candidates = new();
+        private readonly ObservableDictionary<string, Info> sectorDict = new();
         public string SelectedSector { get; set; }
-        public string SelectedCandidate { get; set; }
+        public Candidate SelectedCandidate { get; set; }
 
-        public ObservableCollection<string> SectorStringList => sectorDict.sectorsIndex;
+        // Public properties for binding to XAML
+        public string SectorTitle { get; set; }
+        public string SectorYear { get; set; }
+        public string SectorMax { get; set; }
+        public Color SectorColor { get; set; }
+        public ObservableCollection<string> SectorStringList => sectorDict.Keys;
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
             dialogs = new(dialogHost);
-            SectorList.ItemsSource = sectorDict.sectorsIndex;
-
-            //NameList.ItemsSource = candidates[0];
         }
 
         private void ThemeButton_Click(object sender, RoutedEventArgs e)
@@ -68,22 +70,31 @@ namespace VotingDatabaseMaker
         {
             // Nút thêm Sector
             _ = sectorDict.Add("Test sector 1", new());
-            if (!candidates.ContainsKey("Test sector 1")) candidates.Add("Test sector 1", new());
+            if (!candidates.Add("Test sector 1", new()))
+            {
+                dialogs.ShowTextDialog("Sector đã tồn tại, vui lòng kiểm tra lại", "OK");
+                return;
+            }
+            SectorList.SelectedItem = "Test sector 1";
             AddCandidateButton.IsEnabled = true;
         }
         private async void AddCandidateButton_Click(object sender, RoutedEventArgs e)
         {
             // Nút thêm ứng cử viên
-            // TODO: Show add dialog here, somehow :)
             bool result = (bool)await dialogHost.ShowDialog(candidateDialog);
             if (result == false) return;
+            if (candidates.GetValue(SelectedSector).ContainsKey(candidateDialog.NameInput))
+            {
+                dialogs.ShowTextDialog("Ứng cử viên đã tồn tại", "OK");
+                return;
+            }
             Candidate candidate = new()
             {
                 Name = candidateDialog.NameInput,
                 Gender = candidateDialog.GenderInput
             };
-            candidates[SelectedSector].Add(candidate.Name, candidate);
-            NameList.ItemsSource = candidates[SelectedSector].Values;
+            candidates.GetValue(SelectedSector).Add(candidate.Name, candidate);
+            NameList.ItemsSource = candidates.GetValue(SelectedSector).Values.ToList();
         }
 
         private void SectorEditButton_Click(object sender, RoutedEventArgs e)
@@ -111,9 +122,9 @@ namespace VotingDatabaseMaker
 
         private void SectorList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            SectorEditButton.IsEnabled = true;
-            if (SelectedSector == null)
-                SectorEditButton.IsEnabled = false;
+            // TODO: add enable and disable sector property here
+            NameList.ItemsSource = candidates.GetValue(SelectedSector).Values.ToList();
+            //if (SelectedSector == null)
         }
         private void CandidateList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -125,7 +136,6 @@ namespace VotingDatabaseMaker
                 CandidateEditButton.IsEnabled = false;
                 return;
             };
-            SelectedCandidate = ((Candidate)selectedItem).Name;
         }
         private void SectorList_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
---
+++ VotingDatabaseMaker/ObservableDictionary.cs
@@ -0,0 +1,69 @@
+﻿using VotingPC;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VotingDatabaseMaker
+{
+    /// <summary>
+    /// A unique class that tries to emulate Dictionary with List and ObservableCollection to allow binding with ListView
+    /// </summary>
+    public class ObservableDictionary<TKey, TValue>
+    {
+        // Public property
+        /// <summary>
+        /// List of values
+        /// </summary>
+        public ObservableCollection<TValue> Values { get; } = new();
+        /// <summary>
+        /// List of keys
+        /// </summary>
+        public ObservableCollection<TKey> Keys { get; } = new();
+
+        /// <summary>
+        /// New ObservaleDictionary
+        /// </summary>
+        public ObservableDictionary() { }
+
+        /// <summary>
+        /// Get item from key
+        /// </summary>
+        /// <param name="key">Sector ID / name</param>
+        /// <returns></returns>
+        public TValue GetValue(TKey key)
+        {
+            return Values[Keys.IndexOf(key)];
+        }
+
+        /// <summary>
+        /// Add key and value pair to the collection
+        /// </summary>
+        /// <param name="key">Key of added item</param>
+        /// <param name="value">Item to add</param>
+        /// <returns>false if not added (item already exists), true otherwise</returns>
+        public bool Add(TKey key, TValue value)
+        {
+            if (Keys.Contains(key)) return false;
+            Keys.Add(key);
+            Values.Add(value);
+            return true;
+        }
+
+        /// <summary>
+        /// Remove item with given index
+        /// </summary>
+        /// <param name="index">index of item to remove</param>
+        /// <returns>true if removed, false if item not found</returns>
+        public bool Remove(TKey index)
+        {
+            int indexToRemove = Keys.IndexOf(index);
+            if (indexToRemove == -1) return false;
+            Values.RemoveAt(indexToRemove);
+            Keys.RemoveAt(indexToRemove);
+            return true;
+        }
+    }
+}
