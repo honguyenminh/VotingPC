45cb9582f72bb9ec2c0e62af01a1f04483b83434
Hồ Nguyên Minh <honguyenminh@outlook.com>
2021-11-06 10:24:12 +0700

    Add Boilerplate code for main window.

---
+++ VotingPCNew/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf.Transitions;
+
+namespace VotingPCNew
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            SQLitePCL.Batteries_V2.Init();
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+
+        // Transition methods
+        /// <summary>
+        /// Move to next Slide
+        /// </summary>
+        private void NextPage()
+        {
+            Transitioner.MoveNextCommand.Execute(0, TransitionerObj);
+        }
+        /// <summary>
+        /// Move to previous Slide
+        /// </summary>
+        private void PreviousPage()
+        {
+            Transitioner.MovePreviousCommand.Execute(0, TransitionerObj);
+        }
+    }
+}
