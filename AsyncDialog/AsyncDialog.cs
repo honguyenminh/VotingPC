6a004af18388e3c0582dd216f955163140da8fb3
Hồ Nguyên Minh <honguyenminh@outlook.com>
2021-11-08 08:47:17 +0700

    Added Confirm dialog that extends text dialog. Added documentation for current dialogs.

--- AsyncDialog/AsyncDialog.cs
+++ AsyncDialog/AsyncDialog.cs
@@ -12,7 +12,6 @@ namespace AsyncDialog
     /// </summary>
     public class AsyncDialog
     {
-        // TODO: add documentation
         private readonly DialogHost dialogHost;
 
         private bool isOpen;
@@ -29,29 +28,46 @@ namespace AsyncDialog
         }
 
         /// <summary>
-        /// Show a text dialog with a button to close it
+        /// Show a text dialog with a button to close it and optional title
         /// </summary>
         /// <param name="text">Text to show</param>
         /// <param name="title">(Optional) Title of dialog</param>
         /// <param name="buttonLabel">(Optional) Custom label for button, default is "OK"</param>
-        /// <param name="scaleFactor">Scale factor to scale the dialog</param>
-        public async Task ShowTextDialog(string text, string title = null, string buttonLabel = "OK", double scaleFactor = 1)
+        /// <param name="scaleFactor">(Optional) Scale factor to scale the dialog</param>
+        public async Task ShowTextDialog(string text, string title = null, string buttonLabel = "OK",  double scaleFactor = 1)
         {
             textDialog.Text = text;
             textDialog.Title= title;
             textDialog.SetScaling(scaleFactor);
             textDialog.ButtonLabel = buttonLabel;
+            textDialog.EnableLeftButton = false;
             _ = await dialogHost.ShowDialog(textDialog);
         }
-
-        public async Task ShowConfirmDialog(string text)
+        /// <summary>
+        /// Show a text dialog with an optional title and two button which CLOSES DIALOG ON CLICK
+        /// </summary>
+        /// <param name="text">Text to show</param>
+        /// <param name="title"></param>
+        /// <param name="leftButtonLabel"></param>
+        /// <param name="rightButtonLabel"></param>
+        /// <param name="scaleFactor"></param>
+        /// <returns></returns>
+        public async Task<bool> ShowConfirmTextDialog(string text, string title = null, string leftButtonLabel = "Cancel", string rightButtonLabel = "OK", double scaleFactor = 1)
         {
-
+            textDialog.Text = text;
+            textDialog.Title = title;
+            textDialog.SetScaling(scaleFactor);
+            textDialog.EnableLeftButton = true;
+            textDialog.LeftButtonLabel = leftButtonLabel;
+            textDialog.ButtonLabel = rightButtonLabel;
+            return (bool)await dialogHost.ShowDialog(textDialog);
         }
 
         /// <summary>
         /// Open a dialog with loading animation
+        /// This does not await until the dialog stops, please call <see cref="CloseDialog()">CloseDialog()</see> to close it
         /// </summary>
+        /// 
         public void ShowLoadingDialog()
         {
             if (isOpen) CloseDialog();
@@ -60,7 +76,7 @@ namespace AsyncDialog
         }
 
         /// <summary>
-        /// Close dialog
+        /// Close dialog, to be used with <see cref="ShowLoadingDialog">ShowLoadingDialog()</see>
         /// </summary>
         public void CloseDialog()
         {
