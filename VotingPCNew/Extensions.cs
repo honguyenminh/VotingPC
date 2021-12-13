05c37fdc4cdb1a613f629484a3cb7251e09be884
Hồ Nguyên Minh <honguyenminh@outlook.com>
2021-12-13 19:57:54 +0700

    Add extension methods.

---
+++ VotingPCNew/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace VotingPCNew
+{
+    internal static class Extensions
+    {
+        private static readonly ImageSourceConverter imageConverter = new();
+        public static void SetConfig(this TextBlock textBlock, TextConfig config)
+        {
+            textBlock.Text = config.text ?? "";
+            if (config.size != -1) textBlock.FontSize = config.size;
+            textBlock.FontStyle = config.style;
+            textBlock.FontWeight = config.weight;
+        }
+        public static void SetSource(this Image image, string path)
+        {
+            image.Source = (ImageSource)imageConverter.ConvertFromString(path);
+        }
+        public static string ToTitleCase(this string str)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str);
+        }
+    }
+}
