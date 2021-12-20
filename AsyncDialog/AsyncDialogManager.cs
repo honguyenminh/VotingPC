4369346f0b4068da8673c6de546cc5936149e54b
Hồ Nguyên Minh <honguyenminh@outlook.com>
2021-12-20 15:30:12 +0700

    Upgrade all PJs to .NET 6. Bump all Nuget packages' versions.

--- AsyncDialog/AsyncDialogManager.cs
+++ AsyncDialog/AsyncDialogManager.cs
@@ -2,126 +2,124 @@
 using System;
 using System.Threading.Tasks;
 
-namespace AsyncDialog
+namespace AsyncDialog;
+/// <summary>
+/// Helper class to manage and show frequently used dialog
+/// in MaterialDesignInXaml DialogHost
+/// </summary>
+public class AsyncDialogManager
 {
+    private const int DialogDelayDuration = 400; 
+
+    private readonly DialogHost _dialogHost;
+    private bool _isOpen;
+    private double _scaleFactor = 1;
+    private readonly TextDialog _textDialog = new();
+    private readonly PasswordDialog _passwordDialog = new();
+    private readonly LoadingDialog _loadingDialog = new();
+
     /// <summary>
-    /// Helper class to manage and show frequently used dialog
-    /// in MaterialDesignInXaml DialogHost
+    /// Create a new AsyncDialog instance
     /// </summary>
-    public class AsyncDialogManager
+    /// <param name="dialogHost">The DialogHost instance from MDIX inside your UI that you want dialogs to open in</param>
+    public AsyncDialogManager(DialogHost dialogHost)
     {
-        private const int DialogDelayDuration = 400; 
+        _dialogHost = dialogHost;
+    }
 
-        private readonly DialogHost _dialogHost;
-        private bool _isOpen;
-        private double _scaleFactor = 1;
-        private readonly TextDialog _textDialog = new();
-        private readonly PasswordDialog _passwordDialog = new();
-        private readonly LoadingDialog _loadingDialog = new();
+    /// <summary>
+    /// Change the factor used to scale the dialogs
+    /// </summary>
+    public double ScaleFactor
+    {
+        get => _scaleFactor;
+        set
+        {
+            _scaleFactor = value;
+            _textDialog.ScaleFactor = value;
+            _passwordDialog.ScaleFactor = value;
+            _loadingDialog.ScaleFactor = value;
+        }
+    }
 
-        /// <summary>
-        /// Create a new AsyncDialog instance
-        /// </summary>
-        /// <param name="dialogHost">The DialogHost instance from MDIX inside your UI that you want dialogs to open in</param>
-        public AsyncDialogManager(DialogHost dialogHost)
-        {
-            _dialogHost = dialogHost;
-        }
+    /// <summary>
+    /// Show a text dialog with a button to close it and optional title
+    /// </summary>
+    /// <param name="text">Text to show</param>
+    /// <param name="title">(Optional) Title of the dialog</param>
+    /// <param name="buttonLabel">(Optional) Custom label for button, default is "OK"</param>
+    public async Task ShowTextDialog(string text, string title = null, string buttonLabel = "OK")
+    {
+        _textDialog.Text = text;
+        _textDialog.Title = title;
+        _textDialog.ButtonLabel = buttonLabel;
+        _textDialog.EnableLeftButton = false;
+        _ = await _dialogHost.ShowDialog(_textDialog);
+    }
+    /// <summary>
+    /// Show a text dialog with an optional title and two button which CLOSES DIALOG ON CLICK
+    /// </summary>
+    /// <param name="text">Text to show</param>
+    /// <param name="title">Title of the dialog</param>
+    /// <param name="leftButtonLabel">Custom label for left button, default is "CANCEL"</param>
+    /// <param name="rightButtonLabel">Custom label for right button, default is "OK"</param>
+    /// <returns><see langword="true"/> if user clicked right button, <see langword="false"/> otherwise</returns>
+    public async Task<bool> ShowConfirmTextDialog(string text, string title = null, string leftButtonLabel = "CANCEL", string rightButtonLabel = "OK")
+    {
+        _textDialog.Text = text;
+        _textDialog.Title = title;
+        _textDialog.EnableLeftButton = true;
+        _textDialog.LeftButtonLabel = leftButtonLabel;
+        _textDialog.ButtonLabel = rightButtonLabel;
+        return (bool)(await _dialogHost.ShowDialog(_textDialog))!;
+    }
 
-        /// <summary>
-        /// Change the factor used to scale the dialogs
-        /// </summary>
-        public double ScaleFactor
-        {
-            get => _scaleFactor;
-            set
-            {
-                _scaleFactor = value;
-                _textDialog.ScaleFactor = value;
-                _passwordDialog.ScaleFactor = value;
-                _loadingDialog.ScaleFactor = value;
-            }
-        }
+    /// <summary>
+    /// Show a password dialog
+    /// </summary>
+    /// <param name="title">Title of the dialog</param>
+    /// <param name="passwordBoxLabel">Label of the password box</param>
+    /// <param name="passwordBoxHelperText">Helper text under password box</param>
+    /// <param name="confirmButtonLabel">Custom label for confirm button, default is "OK"</param>
+    /// <param name="cancelButtonLabel">Custom label for cancel button, default is "CANCEL"</param>
+    /// <returns><see langword="null"/> if user cancelled. Else return the password.</returns>
+    public async Task<string> ShowPasswordDialog(string title, string passwordBoxLabel,
+        string passwordBoxHelperText = null, string confirmButtonLabel = "OK", string cancelButtonLabel = "CANCEL")
+    {
+        _passwordDialog.Title = title;
+        _passwordDialog.PasswordBoxLabel = passwordBoxLabel;
+        _passwordDialog.PasswordBoxHelperText = passwordBoxHelperText;
+        _passwordDialog.ConfirmButtonLabel = confirmButtonLabel;
+        _passwordDialog.CancelButtonLabel = cancelButtonLabel;
+        return (string)await _dialogHost.ShowDialog(_passwordDialog);
+    }
 
-        /// <summary>
-        /// Show a text dialog with a button to close it and optional title
-        /// </summary>
-        /// <param name="text">Text to show</param>
-        /// <param name="title">(Optional) Title of the dialog</param>
-        /// <param name="buttonLabel">(Optional) Custom label for button, default is "OK"</param>
-        public async Task ShowTextDialog(string text, string title = null, string buttonLabel = "OK")
-        {
-            _textDialog.Text = text;
-            _textDialog.Title = title;
-            _textDialog.ButtonLabel = buttonLabel;
-            _textDialog.EnableLeftButton = false;
-            _ = await _dialogHost.ShowDialog(_textDialog);
-        }
-        /// <summary>
-        /// Show a text dialog with an optional title and two button which CLOSES DIALOG ON CLICK
-        /// </summary>
-        /// <param name="text">Text to show</param>
-        /// <param name="title">Title of the dialog</param>
-        /// <param name="leftButtonLabel">Custom label for left button, default is "CANCEL"</param>
-        /// <param name="rightButtonLabel">Custom label for right button, default is "OK"</param>
-        /// <returns><see langword="true"/> if user clicked right button, <see langword="false"/> otherwise</returns>
-        public async Task<bool> ShowConfirmTextDialog(string text, string title = null, string leftButtonLabel = "CANCEL", string rightButtonLabel = "OK")
-        {
-            _textDialog.Text = text;
-            _textDialog.Title = title;
-            _textDialog.EnableLeftButton = true;
-            _textDialog.LeftButtonLabel = leftButtonLabel;
-            _textDialog.ButtonLabel = rightButtonLabel;
-            return (bool)(await _dialogHost.ShowDialog(_textDialog))!;
-        }
+    /// <summary>
+    /// Open a dialog with loading animation and optional text.<br/>
+    /// If a dialog is already opened, only change the text instead.<br/>
+    /// This does not await until the dialog stops, please call <see cref="CloseDialog()"/> to close it
+    /// </summary>
+    /// <param name="text">(Optional) Text to show</param>
+    public void ShowLoadingDialog(string text = null)
+    {
+        _loadingDialog.Text = text;
+        if (_isOpen) return;
+        _ = _dialogHost.ShowDialog(_loadingDialog);
+        _isOpen = true;
+    }
 
-        /// <summary>
-        /// Show a password dialog
-        /// </summary>
-        /// <param name="title">Title of the dialog</param>
-        /// <param name="passwordBoxLabel">Label of the password box</param>
-        /// <param name="passwordBoxHelperText">Helper text under password box</param>
-        /// <param name="confirmButtonLabel">Custom label for confirm button, default is "OK"</param>
-        /// <param name="cancelButtonLabel">Custom label for cancel button, default is "CANCEL"</param>
-        /// <returns><see langword="null"/> if user cancelled. Else return the password.</returns>
-        public async Task<string> ShowPasswordDialog(string title, string passwordBoxLabel,
-            string passwordBoxHelperText = null, string confirmButtonLabel = "OK", string cancelButtonLabel = "CANCEL")
-        {
-            _passwordDialog.Title = title;
-            _passwordDialog.PasswordBoxLabel = passwordBoxLabel;
-            _passwordDialog.PasswordBoxHelperText = passwordBoxHelperText;
-            _passwordDialog.ConfirmButtonLabel = confirmButtonLabel;
-            _passwordDialog.CancelButtonLabel = cancelButtonLabel;
-            return (string)await _dialogHost.ShowDialog(_passwordDialog);
-        }
-
-        /// <summary>
-        /// Open a dialog with loading animation and optional text.<br/>
-        /// If a dialog is already opened, only change the text instead.<br/>
-        /// This does not await until the dialog stops, please call <see cref="CloseDialog()"/> to close it
-        /// </summary>
-        /// <param name="text">(Optional) Text to show</param>
-        public void ShowLoadingDialog(string text = null)
-        {
-            _loadingDialog.Text = text;
-            if (_isOpen) return;
-            _ = _dialogHost.ShowDialog(_loadingDialog);
-            _isOpen = true;
-        }
-
-        /// <summary>
-        /// Close dialog
-        /// </summary>
-        /// <remarks>
-        /// Intended to be used with <see cref="ShowLoadingDialog"/>
-        /// </remarks>
-        /// <exception cref="NullReferenceException">Thrown if Identifier of dialog host is null</exception>
-        public async Task CloseDialog()
-        {
-            if (!_isOpen) return;
-            DialogHost.Close(_dialogHost.Identifier!);
-            _isOpen = false;
-            await Task.Delay(DialogDelayDuration);
-        }
+    /// <summary>
+    /// Close dialog
+    /// </summary>
+    /// <remarks>
+    /// Intended to be used with <see cref="ShowLoadingDialog"/>
+    /// </remarks>
+    /// <exception cref="NullReferenceException">Thrown if Identifier of dialog host is null</exception>
+    public async Task CloseDialog()
+    {
+        if (!_isOpen) return;
+        DialogHost.Close(_dialogHost.Identifier!);
+        _isOpen = false;
+        await Task.Delay(DialogDelayDuration);
     }
 }
