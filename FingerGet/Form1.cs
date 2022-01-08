7015311fd734e7825e50bafa71b6b78464e91ae1
Hồ Nguyên Minh <honguyenminh@outlook.com>
2022-01-08 15:08:58 +0700

    Refactor fingerget

--- FingerGet/Form1.cs
+++ FingerGet/Form1.cs
@@ -1,144 +1,141 @@
 ﻿using System.IO.Ports;
 
-namespace FingerGet
+namespace FingerGet;
+
+public partial class MainForm : Form
 {
-    public partial class MainForm : Form
+    private static SerialPort s_serial;
+    private static bool s_isWaitingForSerial;
+    public MainForm()
     {
-        private static SerialPort serial;
-        private static bool isWaitingForSerial;
-        public MainForm()
+        InitializeComponent();
+        submitButton.Enabled = false;
+        Init();
+    }
+    private async void Init()
+    {
+        await Task.Delay(200);
+        while (s_serial == null) await Task.Run(GetArduinoCOMPort);
+        status.Text = "Đã tìm thấy Arduino tại " + s_serial.PortName;
+        status.ForeColor = Color.Green;
+        submitButton.Enabled = true;
+        s_isWaitingForSerial = true;
+        WaitForSerial();
+        CheckArduinoStatus();
+    }
+    private async void CheckArduinoStatus()
+    {
+        while (true)
         {
-            InitializeComponent();
-            submitButton.Enabled = false;
-            Init();
-        }
-        private async void Init()
-        {
-            await Task.Delay(200);
-            while (serial == null) await Task.Run(GetArduinoCOMPort);
-            status.Text = "Đã tìm thấy Arduino tại " + serial.PortName;
-            status.ForeColor = Color.Green;
-            submitButton.Enabled = true;
-            isWaitingForSerial = true;
-            WaitForSerial();
-            CheckArduinoStatus();
-        }
-        private async void CheckArduinoStatus()
-        {
-            while (true)
+            await Task.Delay(800);
+            if (!s_serial.IsOpen)
             {
-                await Task.Delay(800);
-                if (!serial.IsOpen)
-                {
-                    await Disconnected();
-                    Init();
-                    break;
-                }
+                await Disconnected();
+                Init();
+                break;
             }
         }
-        private async Task Disconnected()
-        {
-            await Task.Delay(10);
-            status.Text = "Đã ngắt kết nối Arduino, vui lòng kết nối lại";
-            status.ForeColor = Color.Red;
-            submitButton.Enabled = false;
-            isWaitingForSerial = false;
-            serial.Dispose();
-            serial = null;
-        }
+    }
+    private async Task Disconnected()
+    {
+        await Task.Delay(10);
+        status.Text = "Đã ngắt kết nối Arduino, vui lòng kết nối lại";
+        status.ForeColor = Color.Red;
+        submitButton.Enabled = false;
+        s_isWaitingForSerial = false;
+        s_serial.Dispose();
+        s_serial = null;
+    }
 
-        private async void WaitForSerial()
-        {
-            await Task.Delay(100);
-            while (isWaitingForSerial && serial.IsOpen)
-            {
-                try
-                {
-                    string input = await Task.Run(serial.ReadLine);
-                    if (input == "Internal done")
-                    {
-                        submitButton.Enabled = true;
-                        continue;
-                    }
-                    else if (input == "Internal found")
-                    {
-                        continue;
-                    }
-                    serialOutput.AppendText(input + "\r\n");
-                }
-                catch (Exception)
-                {
-                    return;
-                }
-            }
-        }
-
-        private void SubmitButton_Click(object sender, EventArgs e)
+    private async void WaitForSerial()
+    {
+        await Task.Delay(100);
+        while (s_isWaitingForSerial && s_serial.IsOpen)
         {
             try
             {
-                serial.Write("S");
-                submitButton.Enabled = false;
+                string input = await Task.Run(s_serial.ReadLine);
+                if (input == "Internal done")
+                {
+                    submitButton.Enabled = true;
+                    continue;
+                }
+                else if (input == "Internal found")
+                {
+                    continue;
+                }
+                serialOutput.AppendText(input + "\r\n");
             }
-            catch { _ = Disconnected(); }
-        }
-        private static void GetArduinoCOMPort()
-        {
-            string[] COMPorts = SerialPort.GetPortNames();
-
-            foreach (string COMPort in COMPorts)
+            catch (Exception)
             {
-                SerialPort serialPort = new(COMPort, 115200)
+                return;
+            }
+        }
+    }
+
+    private void SubmitButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            s_serial.Write("S");
+            submitButton.Enabled = false;
+        }
+        catch { _ = Disconnected(); }
+    }
+    private static void GetArduinoCOMPort()
+    {
+        string[] COMPorts = SerialPort.GetPortNames();
+
+        foreach (string COMPort in COMPorts)
+        {
+            SerialPort serialPort = new(COMPort, 115200)
+            {
+                NewLine = "\r\n",
+                Encoding = System.Text.Encoding.UTF8
+            };
+            try
+            {
+                serialPort.Open();
+                serialPort.Write("F");
+                for (int i = 0; i < 10; i++)
                 {
-                    NewLine = "\r\n",
-                    Encoding = System.Text.Encoding.UTF8
-                };
-                try
-                {
-                    serialPort.Open();
-                    serialPort.Write("F");
-                    for (int i = 0; i < 10; i++)
-                    {
-                        Thread.Sleep(100);
-                        if (serialPort.BytesToRead == 0)
-                        {
-                            serialPort.Write("F");
-                        }
-                        else break;
-                    }
+                    Thread.Sleep(100);
                     if (serialPort.BytesToRead == 0)
                     {
-                        serialPort.Dispose();
-                        continue;
+                        serialPort.Write("F");
                     }
-                    string response = serialPort.ReadLine();
-                    if (response == "Internal found")
-                    {
-                        serial = serialPort;
-                        serial.DiscardInBuffer();
-                        return;
-                    }
+                    else break;
                 }
-                catch (Exception) { }
-
-                serialPort.Dispose();
+                if (serialPort.BytesToRead == 0)
+                {
+                    serialPort.Dispose();
+                    continue;
+                }
+                string response = serialPort.ReadLine();
+                if (response == "Internal found")
+                {
+                    s_serial = serialPort;
+                    s_serial.DiscardInBuffer();
+                    return;
+                }
             }
-        }
+            catch (Exception) { }
 
-        private void Form_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            isWaitingForSerial = false;
-            if (serial != null)
-            {
-                serial.Write("C"); // App closed signal
-                if (serial.IsOpen) serial.Close();
-                serial.Dispose();
-            }
+            serialPort.Dispose();
         }
+    }
 
-        private void ClearButton_Click(object sender, EventArgs e)
-        {
-            serialOutput.Text = "";
-        }
+    private void Form_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        s_isWaitingForSerial = false;
+        if (s_serial == null) return;
+        s_serial.Write("C"); // App closed signal
+        if (s_serial.IsOpen) s_serial.Close();
+        s_serial.Dispose();
+    }
+
+    private void ClearButton_Click(object sender, EventArgs e)
+    {
+        serialOutput.Text = "";
     }
 }
